67d6dee48b5d06b86e00387f6f660ad95ce383b8
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2019-02-18 19:57:02 +0300

    Some changes

--- public/main.js
+++ public/main.js
@@ -1,12 +1,278 @@
-console.log('lorem ipsum');
+'use strict';
 
-const h1 = document.getElementsByTagName('h1')[0];
+const application = document.getElementById('application');
 
-function click() {
-  console.log('kek');
 
-  h1.innerText = h1.innerText === 'Frontend' ? 'Hello, World!' : 'Frontend';
+function createMenuLink () {
+	const menuLink = document.createElement('a');
+	menuLink.href = menuLink.dataset.href = 'menu';
+
+	menuLink.textContent = 'Back to main menu';
+
+	return menuLink;
 }
 
-const button = document.getElementsByTagName('button')[0];
-button.addEventListener('click', click);
+function createMenu () {
+	const menuSection = document.createElement('section');
+	menuSection.dataset.sectionName = 'menu';
+
+	const logo = document.createElement('div');
+	logo.id = 'logo';
+	const logoHeader = document.createElement('h1');
+	logoHeader.textContent = 'Our game';
+
+	logo.appendChild(logoHeader);
+
+
+	const main = document.createElement('div');
+	main.id = 'main';
+	const mainInner = document.createElement('div');
+
+	main.appendChild(mainInner);
+
+	const titles = {
+		sign_in: 'Sign in',
+		sign_up: 'Sign up',
+		leaders: 'Leaders',
+		me: 'Profile'
+	};
+
+
+	Object.entries(titles).forEach(function (entry) {
+		const href = entry[ 0 ];
+		const title = entry[ 1 ];
+
+		const a = document.createElement('a');
+		a.href = href;
+		a.dataset.href = href;
+		a.textContent = title;
+		a.classList.add('menu-button');
+
+		mainInner.appendChild(a);
+	});
+
+
+	menuSection.appendChild(logo);
+	menuSection.appendChild(main);
+
+	application.appendChild(menuSection);
+}
+
+function createSignIn () {
+	const signInSection = document.createElement('section');
+	signInSection.dataset.sectionName = 'sign_in';
+
+	const header = document.createElement('h1');
+	header.textContent = 'Sign In';
+
+
+	const form = document.createElement('form');
+
+	const inputs = [
+		{
+			name: 'email',
+			type: 'email',
+			placeholder: 'Email'
+		},
+		{
+			name: 'password',
+			type: 'password',
+			placeholder: 'Password'
+		},
+		{
+			name: 'submit',
+			type: 'submit'
+		}
+	];
+
+	inputs.forEach(function (item) {
+		const input = document.createElement('input');
+
+		input.name = item.name;
+		input.type = item.type;
+
+		input.placeholder = item.placeholder;
+
+		form.appendChild(input);
+		form.appendChild(document.createElement('br'));
+	});
+
+	signInSection.appendChild(header);
+	signInSection.appendChild(form);
+	signInSection.appendChild(createMenuLink());
+
+	application.appendChild(signInSection);
+}
+
+function createSignUp () {
+	const signUpSection = document.createElement('section');
+	signUpSection.dataset.sectionName = 'sign_in';
+
+	const header = document.createElement('h1');
+	header.textContent = 'Sign Up';
+
+
+	const form = document.createElement('form');
+
+	const inputs = [
+		{
+			name: 'email',
+			type: 'email',
+			placeholder: 'Email'
+		},
+		{
+			name: 'age',
+			type: 'number',
+			placeholder: 'Your Age'
+		},
+		{
+			name: 'password',
+			type: 'password',
+			placeholder: 'Password'
+		},
+		{
+			name: 'password_repeat',
+			type: 'password',
+			placeholder: 'Repeat Password'
+		},
+		{
+			name: 'submit',
+			type: 'submit'
+		}
+	];
+
+	inputs.forEach(function (item) {
+		const input = document.createElement('input');
+
+		input.name = item.name;
+		input.type = item.type;
+
+		input.placeholder = item.placeholder;
+
+		form.appendChild(input);
+		form.appendChild(document.createElement('br'));
+	});
+
+	signUpSection.appendChild(header);
+	signUpSection.appendChild(form);
+	signUpSection.appendChild(createMenuLink());
+
+	application.appendChild(signUpSection);
+}
+
+function createLeaderboard (users) {
+	const leaderboardSection = document.createElement('section');
+	leaderboardSection.dataset.sectionName = 'leaderboard';
+
+	const header = document.createElement('h1');
+	header.textContent = 'Leaders';
+
+	leaderboardSection.appendChild(header);
+	leaderboardSection.appendChild(createMenuLink());
+	leaderboardSection.appendChild(document.createElement('br'));
+
+	if (users) {
+		const table = document.createElement('table');
+		const thead = document.createElement('thead');
+		thead.innerHTML = `
+		<tr>
+			<th>Email</th>
+			<th>Age</th>
+			<th>Score</th>
+		</th>
+		`;
+		const tbody = document.createElement('tbody');
+
+		table.appendChild(thead);
+		table.appendChild(tbody);
+		table.border = 1;
+		table.cellSpacing = table.cellPadding = 0;
+
+		users.forEach(function (user) {
+			const email = user.email;
+			const age = user.age;
+			const score = user.score;
+
+			const tr = document.createElement('tr');
+			const tdEmail = document.createElement('td');
+			const tdAge = document.createElement('td');
+			const tdScore = document.createElement('td');
+
+			tdEmail.textContent = email;
+			tdAge.textContent = age;
+			tdScore.textContent = score;
+
+			tr.appendChild(tdEmail);
+			tr.appendChild(tdAge);
+			tr.appendChild(tdScore);
+
+			tbody.appendChild(tr);
+
+			leaderboardSection.appendChild(table);
+		});
+	} else {
+
+	}
+
+	application.appendChild(leaderboardSection);
+}
+
+function createProfile (me) {
+	const profileSection = document.createElement('section');
+	profileSection.dataset.sectionName = 'profile';
+
+	const header = document.createElement('h1');
+	header.textContent = 'Profile';
+
+	profileSection.appendChild(header);
+	profileSection.appendChild(createMenuLink());
+
+	if (me) {
+		const p = document.createElement('p');
+
+		const div1 = document.createElement('div');
+		div1.textContent = `Email ${me.email}`;
+		const div2 = document.createElement('div');
+		div2.textContent = `Age ${me.age}`;
+		const div3 = document.createElement('div');
+		div3.textContent = `Score ${me.score}`;
+
+		p.appendChild(div1);
+		p.appendChild(div3);
+		p.appendChild(div3);
+
+		profileSection.appendChild(p);
+	} else {
+
+	}
+
+	application.appendChild(profileSection);
+}
+
+const functions = {
+	menu: createMenu,
+	sign_in: createSignIn,
+	sign_up: createSignUp,
+	leaders: createLeaderboard,
+	me: createProfile
+};
+
+createMenu();
+
+application.addEventListener('click', function (event) {
+	if (!(event.target instanceof HTMLAnchorElement)) {
+		return;
+	}
+
+	event.preventDefault();
+	const link = event.target;
+
+	console.log({
+		href: link.href,
+		dataHref: link.dataset.href
+	});
+
+	application.innerHTML = '';
+
+	functions[ link.dataset.href ]();
+});
